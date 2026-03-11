23a8b279c8452d839720b740582eea98eb3350a9
Jessica Paradise <jessica.paradise@protonmail.com>
2026-03-10 19:11:22 -0700

    Add ESP32 sketch for WiFi and HTTP color detection

---
+++ esp32_sketch.ino
@@ -0,0 +1,57 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+
+const char* ssid = "eero";
+const char* password = "4Flying247";
+const char* apiUrl = "https://8c8xzuvoj1.execute-api.us-east-2.amazonaws.com/sentiment";
+
+// LED pins
+#define RED_PIN 27
+#define YELLOW_PIN 26
+#define GREEN_PIN 25
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(YELLOW_PIN, OUTPUT);
+  pinMode(GREEN_PIN, OUTPUT);
+
+  // Connect to WiFi
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nConnected!");
+}
+
+void loop() {
+  if (WiFi.status() == WL_CONNECTED) {
+    HTTPClient http;
+    http.begin(apiUrl);
+    int httpCode = http.GET();
+
+    if (httpCode == 200) {
+      String payload = http.getString();
+      Serial.println(payload);
+
+      // Simple color detection
+      if (payload.indexOf("RED") > 0) {
+        digitalWrite(RED_PIN, HIGH);
+        digitalWrite(YELLOW_PIN, LOW);
+        digitalWrite(GREEN_PIN, LOW);
+      } else if (payload.indexOf("YELLOW") > 0) {
+        digitalWrite(RED_PIN, LOW);
+        digitalWrite(YELLOW_PIN, HIGH);
+        digitalWrite(GREEN_PIN, LOW);
+      } else {
+        digitalWrite(RED_PIN, LOW);
+        digitalWrite(YELLOW_PIN, LOW);
+        digitalWrite(GREEN_PIN, HIGH);
+      }
+    }
+    http.end();
+  }
+  delay(300000); // Check every 5 minutes
